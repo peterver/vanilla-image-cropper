767a6710a364718452088ca40736426da70f9fce
Peter Vermeulen <nmb.peterv@gmail.com>
2015-08-01 14:49:35 +0200

    Added plumber for error handling to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,25 +16,32 @@ var paths = {
     scss_dest   : 'build/'
 };
 
+var onError = function (err) {
+  $.notify({title: 'Gulp', message: 'Error: <%= err.message %>'})(err);
+  this.emit('end');
+};
+
 //  Javascript
 gulp.task('js', function (cb) {
-    return gulp.src(paths.js)
-        .pipe($.jshint.reporter('jshint-stylish'))
-        .pipe($.jshint.reporter('fail'))
-        .pipe($.notify({title: 'jshint', message: 'jshint - passed'}))
-        .pipe($.uglify())
-        .pipe($.rename({suffix: '.min'}))
-        .pipe(gulp.dest(paths.js_dest));
+  return gulp.src(paths.js)
+    .pipe($.plumber({errorHandler: onError}))
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'))
+    .pipe($.notify({title: 'jshint', message: 'jshint - passed'}))
+    .pipe($.uglify())
+    .pipe($.rename({suffix: '.min'}))
+    .pipe(gulp.dest(paths.js_dest));
 });
 
 //  SASS
 gulp.task('scss', function (cb) {
-    return gulp.src(paths.scss)
-        .pipe($.scssLint({'config': '.scss-lint.yml'}))
-        .pipe($.sass({outputStyle: 'compressed'}))
-        .pipe($.autoprefixer())
-        .pipe($.rename({suffix: '.min'}))
-        .pipe(gulp.dest(paths.scss_dest));
+  return gulp.src(paths.scss)
+    .pipe($.plumber({errorHandler: onError}))
+    .pipe($.scssLint({'config': '.scss-lint.yml'}))
+    .pipe($.sass({outputStyle: 'compressed'}))
+    .pipe($.autoprefixer())
+    .pipe($.rename({suffix: '.min'}))
+    .pipe(gulp.dest(paths.scss_dest));
 });
 
 //  Just throw a quick notification in the CLI when everything was built.
