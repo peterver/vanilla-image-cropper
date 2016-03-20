9d9b9dc9eb23b0a69abf715cd4aa3a058f242aff
Peter Vermeulen <nmb.peterv@gmail.com>
2016-03-20 23:50:38 +0100

    adjust karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,6 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'src/js/**/*.js': 'coverage'
     },
 
     plugins: [
@@ -43,7 +42,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress'],
 
 
     // web server port
