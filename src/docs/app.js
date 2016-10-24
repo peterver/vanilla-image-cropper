e00c94120b34ac44f0eec905ce528c0ffcb8669b
Peter Vermeulen <nmb.peterv@gmail.com>
2016-10-24 23:30:51 +0200

    wip on docs

--- src/docs/app.js
+++ src/docs/app.js
@@ -2,7 +2,8 @@
 
 //  Setup Image cropper
 
-const img_c = new ImageCropper('.test-imagecrop', "http://thecatapi.com/api/images/get?size=med");
+const example_1 = new ImageCropper('.example-1', "./example1.jpg");
+const example_2 = new ImageCropper('.example-2', "./example2.jpg");
 
 //  Highlighting
 hljs.initHighlightingOnLoad();
