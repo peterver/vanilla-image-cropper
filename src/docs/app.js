2a4af13df5d225abf6ee4e3c5e11229d0017cd0b
Peter Vermeulen <nmb.peterv@gmail.com>
2016-10-25 02:00:30 +0200

    update sizing example

--- src/docs/app.js
+++ src/docs/app.js
@@ -2,8 +2,14 @@
 
 //  Setup Image cropper
 
-const example_1 = new ImageCropper('.example-1', "./example1.jpg");
-const example_2 = new ImageCropper('.example-2', "./example2.jpg");
+const example_1 = new ImageCropper('.example-1', "./example1.jpg", {
+    max_width : 300,
+    max_height : 300,
+});
+const example_2 = new ImageCropper('.example-2', "./example2.jpg", {
+    max_width : 300,
+    max_height : 300,
+});
 
 //  Size examples
 
