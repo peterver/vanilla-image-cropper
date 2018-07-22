2e08dffc13965c09f57a906f6e14cb547dfa9e90
Peter Vermeulen <contact@valkyriestudios.be>
2018-07-22 14:01:50 +0200

    Upgrade to gulp v4 and adjust gulpfile / rollup config to work with new version

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,14 @@
 'use strict';
 
 const gulp = require('gulp');
-const runSequence = require('run-sequence');
-
 const sync = require('browser-sync').create();
 
 const rollup = require('gulp-rollup');
 const babel = require('rollup-plugin-babel');
-const eslint = require('rollup-plugin-eslint');
+const { eslint } = require('rollup-plugin-eslint');
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
-const uglify = require('rollup-plugin-uglify');
+const { uglify } = require('rollup-plugin-uglify');
 
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
@@ -47,9 +45,11 @@ const config = {
         return gulp.src(component.src)
         .pipe(
             rollup({
-                entry: component.entry,
-                format: 'iife',
-                moduleName: 'ImageCropper',
+                input: component.entry,
+                output: {
+                    name: 'ImageCropper',
+                    format: 'iife',
+                },
                 plugins: [
                     resolve({
                         jsnext: true,
@@ -93,12 +93,12 @@ const config = {
 //  SOURCE TASKS
 //
 
-    gulp.task('js', () => doRollup(config.js).pipe(gulp.dest('docs/dist')));
-    gulp.task('scss', () => doSCSS(config.scss).pipe(gulp.dest('docs/dist')));
+    gulp.task('js',         () => doRollup(config.js).pipe(gulp.dest('docs/dist')));
+    gulp.task('scss',       () => doSCSS(config.scss).pipe(gulp.dest('docs/dist')));
 
-    gulp.task('docs:js', () => doRollup(config.docs.js));
-    gulp.task('docs:scss', () => doSCSS(config.docs.scss));
-    gulp.task('docs', () => runSequence('docs:js', 'docs:scss'));
+    gulp.task('docs:js',    () => doRollup(config.docs.js));
+    gulp.task('docs:scss',  () => doSCSS(config.docs.scss));
+    gulp.task('docs',       gulp.series('docs:js', 'docs:scss'));
 
 //
 //  BUILD TASKS
@@ -106,20 +106,20 @@ const config = {
 
     gulp.task('reload', () => sync.reload());
 
-    gulp.task('serve', ['dist'], (cb) => {
+    gulp.task('dist', gulp.series('js', 'scss', 'docs'));
+
+    gulp.task('serve', gulp.series('dist', () => {
         sync.init({
             server : ['./docs', './dist'],
             index : 'index.html',
             files : ['./dist/js/imagecrop.min.js', './dist/css/imagecrop.min.css', './docs/index.html', './docs/app.min.js']
         });
 
-        gulp.watch(config.scss.src, () => runSequence('scss', 'reload'));
-        gulp.watch(config.js.src, () => runSequence('js', 'reload'));
-        gulp.watch(config.docs.js.src, () => runSequence('docs:js', 'reload'));
-        gulp.watch(config.docs.scss.src, () => runSequence('docs:scss', 'reload'));
-    });
-
-    gulp.task('dist', (cb) => runSequence('js', 'scss', 'docs', cb));
+        gulp.watch(config.scss.src,         gulp.series('scss', 'reload'));
+        gulp.watch(config.js.src,           gulp.series('js', 'reload'));
+        gulp.watch(config.docs.js.src,      gulp.series('docs:js', 'reload'));
+        gulp.watch(config.docs.scss.src,    gulp.series('docs:scss', 'reload'));
+    }));
 
     //  Wrapper task for building all files, and watching for changes.
-    gulp.task('default', () => runSequence('serve'));
+    gulp.task('default', gulp.series('serve'));
